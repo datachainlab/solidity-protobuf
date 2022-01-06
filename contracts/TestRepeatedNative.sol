98179006ffd2d6d20b321bda14f42904bdbbb701
Masanori Yoshida <masanori.yoshida@datachain.jp>
2022-01-06 10:20:55 +0900

    update tests of repeated fields for testing the feature of packed repeated
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

--- contracts/TestRepeatedNative.sol
+++ contracts/TestRepeatedNative.sol
@@ -7,7 +7,8 @@ contract TestRepeatedNative {
     uint256[] uint256s;
     int64[] sint64s;
     bool bool_field;
-
+    int32[] unpacked_int32s;
+    int32[] packed_int32s;
   }
   mapping(address => Data) public contracts;
 
@@ -27,11 +28,32 @@ contract TestRepeatedNative {
     return contracts[key].sint64s;
   }
 
-  function storeTestRepeated(address key, string memory string_field,
-    uint256[] memory uint256s, int64[] memory sint64s,
-    bool bool_field) public {
-      Data memory data = Data({string_field: string_field, uint256s: uint256s,
-        sint64s: sint64s, bool_field: bool_field});
-      contracts[key] = data;
+
+  function getTestRepeatedUnpackedInt32(address key) public view returns (int32[] memory) {
+    return contracts[key].unpacked_int32s;
+  }
+
+  function getTestRepeatedPackedInt32(address key) public view returns (int32[] memory) {
+    return contracts[key].packed_int32s;
+  }
+
+  function storeTestRepeated(
+    address key,
+    string memory string_field,
+    uint256[] memory uint256s,
+    int64[] memory sint64s,
+    bool bool_field,
+    int32[] memory unpacked_int32s,
+    int32[] memory packed_int32s
+  ) public {
+    Data memory data = Data({
+      string_field: string_field,
+      uint256s: uint256s,
+      sint64s: sint64s,
+      bool_field: bool_field,
+      unpacked_int32s: unpacked_int32s,
+      packed_int32s: packed_int32s
+    });
+    contracts[key] = data;
   }
 }
--- contracts/TestRepeatedTwoPB.sol
+++ contracts/TestRepeatedTwoPB.sol
@@ -33,12 +33,38 @@ contract TestRepeatedTwoPB {
     return data.sint64s;
   }
 
-  function storeTestRepeated(address key, string memory string_field,
-    uint256[] memory uint256s, int64[] memory sint64s,
-    bool bool_field) public {
-      TestRepeatedTwo.Data memory data = TestRepeatedTwo.Data({string_field: string_field, uint256s: uint256s,
-        sint64s: sint64s, bool_field: bool_field});
-      bytes memory encoded = TestRepeatedTwo.encode(data);
-      ProtoBufRuntime.encodeStorage(contracts[key], encoded);
+  function getTestRepeatedUnpackedInt32(address key) public view returns (int32[] memory) {
+    bytes storage location = contracts[key];
+    bytes memory encoded = ProtoBufRuntime.decodeStorage(location);
+    TestRepeatedTwo.Data memory data = TestRepeatedTwo.decode(encoded);
+    return data.unpacked_int32s;
+  }
+
+  function getTestRepeatedPackedInt32(address key) public view returns (int32[] memory) {
+    bytes storage location = contracts[key];
+    bytes memory encoded = ProtoBufRuntime.decodeStorage(location);
+    TestRepeatedTwo.Data memory data = TestRepeatedTwo.decode(encoded);
+    return data.packed_int32s;
+  }
+
+  function storeTestRepeated(
+    address key,
+    string memory string_field,
+    uint256[] memory uint256s,
+    int64[] memory sint64s,
+    bool bool_field,
+    int32[] memory unpacked_int32s,
+    int32[] memory packed_int32s
+  ) public {
+    TestRepeatedTwo.Data memory data = TestRepeatedTwo.Data({
+      string_field: string_field,
+      uint256s: uint256s,
+      sint64s: sint64s,
+      bool_field: bool_field,
+      unpacked_int32s: unpacked_int32s,
+      packed_int32s: packed_int32s
+    });
+    bytes memory encoded = TestRepeatedTwo.encode(data);
+    ProtoBufRuntime.encodeStorage(contracts[key], encoded);
   }
 }
